f88122584be7bdd7e76ac325f61f4ff614d445ed
Gregory Losco <gtlosco@gmail.com>
2019-08-27 02:03:23 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -87,7 +87,7 @@ def can_update_multiple_items_at_once
   5.times do |i|
    Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  movie.all.each do |movie|
+    Movie.all.each do |movie|
     movie.update(title: "A Movie")
   end
 end
